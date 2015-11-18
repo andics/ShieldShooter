ba6ac223d43d3c49eb0be4bfc2e4bc1a06842a96
andics <skokskolelo@abv.bg>
2015-11-18 21:40:33 +0200

    TCP send working...now the receiving...

--- app/src/main/java/com/example/admin/myapplication/Utils.java
+++ app/src/main/java/com/example/admin/myapplication/Utils.java
@@ -8,9 +8,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -32,94 +34,72 @@ public class Utils {
     static String sendData;
     static String receiveData;
 
-    public static PrintWriter outToServer;
-    public static BufferedReader inFromServer;
-
-    private static Socket clientSocket;
-
     private static TextView console;
 
+    public static PrintWriter outToServer;
+    public static Socket clientSocket;
+    public static BufferedReader inFromServer;
     public static void register(InetAddress ip, int port, String name) {
         try {
-            Utils.ip = ip;
-            Utils.port = port;
-            Utils.name = name;
+            clientSocket = new Socket(ip, port);
+                outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+                inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+                Utils.ip=ip;
+                Utils.port=port;
+                Utils.name=name;
 
-            send("reg:" + name);
+                send("reg:" + name);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        receive();
+                    }
+                }).start();
+        }   catch (IOException e) {
+            e.printStackTrace();
+            Log.e("BUG","Socket failed to connect!");
+        }
+    }
+    //That method is called many times after "send("reg:" + name);"
+    public static void send(String str) {
+        outToServer.println(str);
+    }
+    private static void receive() {
+        try{
+            while (true) {
+                if(inFromServer.readLine()!=null) {
+                    String fromServer = inFromServer.readLine();
 
-           new Thread(new Runnable() {
-                @Override
-                public void run() {
-            //        receive();
+                    if (fromServer.startsWith("reg:") && fromServer.length() > 4) {
+                        String[] firstSplit = fromServer.split(":");
+                        Utils.players = Arrays.asList(firstSplit[1].split(","));
+                    }
+
+                    if (fromServer.startsWith("var:")) {
+                        String[] firstSplit = fromServer.split(":");
+                        String[] secondSplit = firstSplit[1].split("-");
+                        Variables.set(secondSplit[0], Integer.parseInt(secondSplit[1]));
+                    }
+
+                    if (fromServer.startsWith("msg:")) {
+                        String[] split = fromServer.split(":");
+                        Utils.append(split[1]);
+                        if (split[1].trim().equals("Starting next round")) {
+                            inGameActivity.activity.doRound();
+                        }
+                    }
+
+                    if (fromServer.startsWith("close")) {
+                        clientSocket.close();
+                        return;
+                    }
                 }
-            }).start();
 
-
-            //new
-
-        } catch (Exception e) {
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-
-    public static void send(final String str) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    clientSocket = new Socket(Utils.ip, Utils.port);
-                    outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
-                    outToServer.println(str);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-            }
-    private static void receive() {
-        while (true) {
-            try {
-                inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                 String fromServer = inFromServer.readLine().trim();
-
-                if (fromServer.startsWith("reg:") && fromServer.length() > 4) {
-                    String[] firstSplit = fromServer.split(":");
-                    Utils.players = Arrays.asList(firstSplit[1].split(","));
-                }
-
-                if (fromServer.startsWith("var:")) {
-                    String[] firstSplit = fromServer.split(":");
-                    String[] secondSplit = firstSplit[1].split("-");
-                    Variables.set(secondSplit[0], Integer.parseInt(secondSplit[1]));
-                }
-
-                if (fromServer.startsWith("msg:")) {
-                    String[] split = fromServer.split(":");
-                    Utils.append(split[1].trim());
-                    if (split[1].trim().equals("Starting next round")) {
-                        inGameActivity.activity.doRound();
-                    }
-                }
-
-                if (fromServer.startsWith("close")) {
-                    clientSocket.close();
-                    return;
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            /*    if(e instanceof SocketException) {
-                    try {
-                        clientSocket = new Socket(InetAddress.getByName("130.204.8.60"), 9876);
-                    } catch (IOException e1) {
-                        e.printStackTrace();
-                    }
-            } */
-        }
-    }
-
 }
 
 
