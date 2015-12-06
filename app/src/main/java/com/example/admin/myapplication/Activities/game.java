9bbc67e680430ffb1d959bf4ea7e3da3bc49f098
andics <skokskolelo@abv.bg>
2015-12-06 23:04:04 +0200

    New game design incoming

---
+++ app/src/main/java/com/example/admin/myapplication/Activities/game.java
@@ -0,0 +1,22 @@
+package com.example.admin.myapplication.Activities;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import com.example.admin.myapplication.R;
+
+public class game extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        TextView tv = (TextView) findViewById(R.id.gameState);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/gothic.TTF");
+        tv.setTypeface(face);
+    }
+
+}
